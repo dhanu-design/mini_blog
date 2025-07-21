29949af8c75ee365bd15746ac272fe5c237f6edc
dhanu-design <chinnuchinna1220@gmail.com>
2025-07-21 10:25:59 +0530

    Update add_post.php

--- add_post.php
+++ add_post.php
@@ -6,13 +6,13 @@
 <body>
     <h1>Add New Post</h1>
     <form action="insert_post.php" method="POST">
-        <label>Title:</label><br>
-        <input type="text" name="title" required><br><br>
+  <label for="title">Title:</label><br>
+  <input type="text" id="title" name="title" required><br><br>
 
-        <label>Content:</label><br>
-        <textarea name="content" rows="5" cols="40" required></textarea><br><br>
+  <label for="content">Content:</label><br>
+  <textarea id="content" name="content" rows="5" cols="40" required></textarea><br><br>
 
-        <input type="submit" value="Add Post">
-    </form>
+  <input type="submit" value="Add Post">
+</form>
 </body>
 </html>
